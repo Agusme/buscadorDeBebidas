c59da4cea5d5b7f80cf145bc872e615607c41917
agusmena <agusmena60@mail.com>
2025-09-02 12:30:04 -0300

    agregamos validaciones con zod, axios para llamar a la api, renderizamos cada categoria de las bebidas

---
+++ src/store/useAppStore.ts
@@ -0,0 +1,7 @@
+import { create } from "zustand";
+import { createRecipesSlice, type RecipeSliceType } from "./recipeSlice";
+import { devtools } from "zustand/middleware";
+
+export const useAppStore= create<RecipeSliceType>()(devtools((...a) =>({
+    ...createRecipesSlice(...a)
+})))
