b03abbddfc1c72f5731b2b4556a8731c4a8ffa24
agusmena <agusmena60@mail.com>
2025-09-02 10:53:59 -0300

    agrego clase en theme para imagen de fondo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,8 @@ import {  NavLink, useLocation } from "react-router-dom";
 export default function Header() {
     const {pathname} = useLocation()
     const isHome = useMemo(()=> pathname === '/',[pathname])
-    console.log(isHome)
-    console.log(pathname)
     return (
-        <header className="bg-slate-800">
+        <header className={isHome ? 'bg-header bg-center bg-cover':'bg-slate-800'} >
             <div className="mx-auto container px-5 py-16">
                 <div className="flex justify-between item-center">
                     <div>
